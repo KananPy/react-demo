08051affc50ec7ed6e82b4a77b95f7fea76948c7
KananPy <kenan.semenderli@gmail.com>
2023-07-08 03:32:05 +0400

    demoreact

---
+++ trendyol/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Header } from "./components/Header"
+
+export class App extends React.Component{
+
+  render(){
+
+
+
+    return (
+      <>
+      <Header/>
+      </>
+      
+    )
+  }
+
+}
---
+++ trendyol/src/components/HeaderInfo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class HeaderInfo extends Component {
+  render() {
+    return (
+        <div className='header-bottom'>
+        <div class="card border-0 bg-primary">
+
+            <div class="card-body text-center">
+                <h5 class="card-title display-1 text-light">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-warning">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    )
+  }
+}
+
